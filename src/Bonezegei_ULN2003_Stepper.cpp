b321b94bcfcc32564792adce52041db22227e9b5
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-02 16:45:29 +0800

    Create Bonezegei_ULN2003_Stepper.cpp

---
+++ src/Bonezegei_ULN2003_Stepper.cpp
@@ -0,0 +1,9 @@
+
+#include "Bonezegei_ULN2003_Stepper.h"
+
+Bonezegei_ULN2003_Stepper::Bonezegei_ULN2003_Stepper(int in1, int in2, int in3, int in4){
+  _in1 = in1;
+  _in2 = in2;
+  _in3 = in3;
+  _in4 = in4;
+}
